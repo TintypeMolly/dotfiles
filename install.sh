c0433addc8b446fbe89ff7a6d85b633ec5c1950b
Oh Jinkyun <tintypemolly@gmail.com>
2016-06-16 20:55:11 +0900

    recursive copy bug on mac resolevd

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-TARGETS=".bashrc .gitconfig .gitignore_global .vim/ .vimrc"
+TARGETS=".bashrc .gitconfig .gitignore_global .vim .vimrc"
 
 for f in $TARGETS
 do
