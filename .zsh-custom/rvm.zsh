eb510e2819e7a8211f3e2ff5630c76f3ae773867
Oh Jinkyun <tintypemolly@gmail.com>
2018-08-05 03:25:15 +0900

    don't append path if rvm dir does not exist

--- .zsh-custom/rvm.zsh
+++ .zsh-custom/rvm.zsh
@@ -1,3 +1,5 @@
-export PATH="$PATH:$HOME/.rvm/bin"
+if [ -d $HOME/.rvm/bin ] then
+    export PATH="$PATH:$HOME/.rvm/bin"
+fi
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 
