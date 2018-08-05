4658505e5e7bc6a2a405c7e48f0d6d8986bdcb8f
Oh Jinkyun <tintypemolly@gmail.com>
2018-08-06 02:10:20 +0900

    fix if statement typo

--- .zsh-custom/rvm.zsh
+++ .zsh-custom/rvm.zsh
@@ -1,4 +1,5 @@
-if [ -d $HOME/.rvm/bin ] then
+if [ -d $HOME/.rvm/bin ]
+then
     export PATH="$PATH:$HOME/.rvm/bin"
 fi
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
